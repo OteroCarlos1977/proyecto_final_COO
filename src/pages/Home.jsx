ac21ea4f45e4aba71e180b3d65b99a3315451214
Carlos Octavio Otero <166446069+OteroCarlos1977@users.noreply.github.com>
2026-05-01 15:09:40 -0300

    Simplifica componente Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
 import Cards from '../componentes/Cards';
 
-// Componente Home que recibe filtros de categoría y una función para agregar productos al carrito
 function Home({ categoryFilter, onAgregarAlCarrito }) {
-    return (
-        <div>
-            {/* Renderiza el componente Cards pasando los props recibidos */}
-            <Cards
-                categoryFilter={categoryFilter}         // Filtra las tarjetas por categoría seleccionada
-                onAgregarAlCarrito={onAgregarAlCarrito} // Función para agregar un producto al carrito
-            />
-        </div>
-    );
+  return (
+    <div>
+      <Cards categoryFilter={categoryFilter} onAgregarAlCarrito={onAgregarAlCarrito} />
+    </div>
+  );
 }
 
 export default Home;
